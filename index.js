d16c55b34131428cd718e5a65d83650476e2a73d
Ghug <ptp112004@gmail.com>
2025-06-24 00:34:27 +0700

    2280602318_PhanTanPhat add create comments

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 const express = require('express');
 const postRouter = require('./routes/posts.js');
+const uploadRouter = require('./routes/uploads.js');
+const commentRouter = require('./routes/comments.js');
 
 const app = express();
 const port = 8080;
 
 app.use('/posts', postRouter);
+app.use('/uploads', uploadRouter);
+app.use('/comments', commentRouter);
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
