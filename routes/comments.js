654b33956e32e12487260fdac56c7e9576a6b3cf
Ben2811 <luanchuong51@gmail.com>
2025-06-24 01:17:53 +0700

    2280601846_ChuongTuLuan add comment moderation

--- routes/comments.js
+++ routes/comments.js
@@ -3,3 +3,13 @@ const router = require('express').Router();
 router.post('/', (req, res) => { 
     res.send('Created a new comment');
 });
+
+router.get('/:id', (req, res) => {
+    const commentId = req.params.id;
+    res.send(`Comment with ID: ${commentId}`);
+})
+
+router.delete('/:id', (req, res) => {
+    const commentId = req.params.id;
+    res.send(`Deleted comment with ID: ${commentId}`);
+});
