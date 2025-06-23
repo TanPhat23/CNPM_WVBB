7bb9cd8c8aacba71f676b3751a10582e99cb2153
Nguyễn Quang Trung <quangtrungnguyeen@gmail.com>
2025-06-24 01:34:00 +0700

    2280603456_NguyenQuangTrung add approve post

--- routes/posts.js
+++ routes/posts.js
@@ -4,5 +4,9 @@ const router = express.Router();
 router.post('/', (req, res) => {
   res.send('Create a new post');
 });
+router.put('/:id', (req, res) => {
+    const PostsId = req.params.id;
+    res.send(`Post with ID: ${PostsId}`);
+})
 
 module.exports = router;
