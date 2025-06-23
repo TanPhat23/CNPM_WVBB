93ae7ff5ed9f7cdf7a6e4beccba49a03da7ce99c
Ghug <ptp112004@gmail.com>
2025-06-24 00:36:18 +0700

    2280602318 add create reports

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const postRouter = require('./routes/posts.js');
 const uploadRouter = require('./routes/uploads.js');
 const commentRouter = require('./routes/comments.js');
+const reportRouter = require('./routes/reports.js');
 
 const app = express();
 const port = 8080;
@@ -9,6 +10,7 @@ const port = 8080;
 app.use('/posts', postRouter);
 app.use('/uploads', uploadRouter);
 app.use('/comments', commentRouter);
+app.use('/reports', reportRouter);
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
